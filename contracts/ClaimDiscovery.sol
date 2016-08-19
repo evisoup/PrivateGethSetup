6c6c487e90fbebc936eec1ecf23b5e28fd958056
hengyi Liu <h225liu@uwaterloo.ca>
2016-08-19 10:40:26 -0400

    adding more tools

---
+++ contracts/ClaimDiscovery.sol
@@ -0,0 +1,39 @@
+contract ClaimDiscovery {
+    
+    // Geth node owner
+    address owner;
+    //the event SC will fire once a zip code is passed to it
+    event SendNotificationToDB( string _zipCode);
+    event ImpactedCustomerID( uint _customerID);
+
+    //SC constructor: set the owner  
+    function ClaimDiscovery(){
+        owner = msg.sender;
+    }
+
+    //the function that front end app js will call, with a zipcode
+    //to trigger ARROW 1(i.e US1)
+    function NotifyDB( string _zipCode)
+        byTheOwner(){
+        SendNotificationToDB(_zipCode);
+    }
+
+    //receive a array of uin(customer ID)
+    //fire off events during iteration to trigger SMS notification
+    //to trigger ARROW 2(i.e US2+US3)
+    function ReceiveFromDB( uint256[] _customerIDs){
+
+        uint len  = _customerIDs.length;
+        for (uint i = 0; i < len; i++){
+           ImpactedCustomerID(_customerIDs[i] );
+        }
+
+    }
+
+    //ensure it's the SC owner calling, not someone else
+    modifier byTheOwner () {
+        if (msg.sender == owner) _
+       
+    }
+
+}
--- pocDep.js
+++ pocDep.js
@@ -1,4 +1,5 @@
 var Web3 = require('web3');
+var fs = require('fs');
 
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 web3.eth.defaultAccount = web3.eth.coinbase;
@@ -6,17 +7,28 @@ web3.eth.defaultAccount = web3.eth.coinbase;
 //console.log(web3.eth.coinbase);
 
 
-var greeterSource = 'contract mortal { /* Define variable owner of the type address*/ address owner; /* this function is executed at initialization and sets the owner of the contract */ function mortal() { owner = msg.sender; } /* Function to recover the funds on the contract */ function kill() { if (msg.sender == owner) selfdestruct(owner); } } contract greeter is mortal { /* define variable greeting of the type string */ string public greeting; uint public counter; /* this runs when the contract is executed */ function greeter(string _greeting) public { greeting = _greeting; counter = 0; } /* main function */ function greet() constant returns (string) { counter = counter + 1; return greeting; } /* main function */ function bye() returns (string) { counter = counter + 1; return greeting; } }';
+//var greeterSource = 'contract mortal { /* Define variable owner of the type address*/ address owner; /* this function is executed at initialization and sets the owner of the contract */ function mortal() { owner = msg.sender; } /* Function to recover the funds on the contract */ function kill() { if (msg.sender == owner) selfdestruct(owner); } } contract greeter is mortal { /* define variable greeting of the type string */ string public greeting; uint public counter; /* this runs when the contract is executed */ function greeter(string _greeting) public { greeting = _greeting; counter = 0; } /* main function */ function greet() constant returns (string) { counter = counter + 1; return greeting; } /* main function */ function bye() returns (string) { counter = counter + 1; return greeting; } }';
+
+var sol = fs.readdirSync('./contracts');
 
 
-var greeterCompiled = web3.eth.compile.solidity(greeterSource);
+sol.forEach(function(file){
+		try {
+			source = fs.readFileSync('./contracts/' + file, 'utf8');
+			greeterCompiled = web3.eth.compile.solidity(source);
 
-//console.log(greeterCompiled)
+		} catch (err) {console.log(err)}
+	});
 
-var _greeting = "Hello World with counter 4!"
+
+
+//var greeterCompiled = web3.eth.compile.solidity(greeterSource);
+
+
+var _greeting = "Hello World with counter 6!"
 var greeterContract = web3.eth.contract(greeterCompiled.greeter.info.abiDefinition);
 
-//console.log(greeterCompiled.greeter.info.abiDefinition);
+//var greeter = web3.eth.contract(greeterCompiled.greeter.info.abiDefinition).at("0xd69dd2f38256a11cba3a8575d08eec5d01cfb7cb")
 
 var greeter = greeterContract.new(_greeting,{from:web3.eth.accounts[0], data: greeterCompiled.greeter.code, gas: 300000}, function(e, contract){
    if(!e) {
@@ -30,3 +42,32 @@ var greeter = greeterContract.new(_greeting,{from:web3.eth.accounts[0], data: gr
 
    }
 })
+
+
+
+
+// compiling tools!!!
+
+// function build() {
+// 	if (!interfaces)
+// 		interfaces = load('interfaces');
+// 	sol = fs.readdirSync('contracts/sol');
+// 	sol.forEach(function(file){
+// 		try {
+// 			source = fs.readFileSync('contracts/sol/' + file, 'utf8');
+// 			compiled = web3.eth.compile.solidity(source);
+// 			Object.keys(compiled).forEach((elem) => {
+// 				code = compiled[elem].code;
+// 				codeHash = web3.sha3(code);
+// 				abi = compiled[elem].info.abiDefinition;
+// 				interfaces[elem] = {
+// 					addresses: [],
+// 					code : code,
+// 					codeHash : codeHash,
+// 					abi : abi,
+// 				};
+// 			});
+// 			store('interfaces', interfaces);
+// 		} catch (err) {console.log(err)}
+// 	});	
+// }
